04182b5a1ee35441b17ff6de814115b69676e804
Kampaku <kampaku@users.noreply.github.com>
2022-08-04 10:23:12 +0300

    move to other folder

---
+++ src/App/App.tsx
@@ -0,0 +1,25 @@
+import '../assets/scss/style.scss';
+
+import { useEffect } from 'react';
+import { Route,Routes } from 'react-router-dom';
+
+import { Header } from '../components';
+import { CountryPage,SearchPage } from '../pages';
+import styles from './App.module.scss';
+
+const App = () => {
+
+  return (
+    <div className={styles.app}>
+      <Header />
+      <div className={styles.container}>
+        <Routes>
+          <Route path='/' element={<SearchPage />}/>
+          <Route path='/country/:name' element={<CountryPage />}/>
+        </Routes>
+      </div>
+    </div>
+  );
+};
+
+export default App;
