c7b2acf7ca00e7af3429d69cf8d721e6669995b5
Kampaku <kampaku@users.noreply.github.com>
2022-08-07 18:45:23 +0300

    add spinner

---
+++ src/components/Spinner/Spinner.tsx
@@ -0,0 +1,9 @@
+import { CgSpinnerTwoAlt } from 'react-icons/cg';
+
+import styles from './Spinner.module.scss';
+
+const Spinner = () => {
+  return <CgSpinnerTwoAlt className={styles.loader} />;
+};
+
+export { Spinner };
