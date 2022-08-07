f7150102a36a4cd2dcdc83eb21459fbe83dd78fc
Kampaku <kampaku@users.noreply.github.com>
2022-08-07 18:54:58 +0300

    add country card

---
+++ src/components/CountryCard/CountryCard.tsx
@@ -0,0 +1,38 @@
+import type { FC } from 'react';
+import { Link } from 'react-router-dom';
+
+import type { Country } from '../../types/types';
+import styles from './CountryCard.module.scss';
+
+type Props = {
+  data: Pick<Country, 'name' | 'population' | 'region' | 'capital' | 'flags'>;
+};
+
+const CountryCard: FC<Props> = ({
+  data: { capital, name, population, region, flags },
+}) => {
+  return (
+    <Link to={`/country/${name.common}`} className={styles.link}>
+      <div className={styles.card}>
+        <img src={flags.svg} alt="flag" className={styles.img} />
+        <div className={styles.info}>
+          <span className={styles.countryName}>{name.common}</span>
+          <span>
+            <span className={styles.category}>Population: </span>
+            {population.toLocaleString()}
+          </span>
+          <span>
+            <span className={styles.category}>Region: </span>
+            {region}
+          </span>
+          <span>
+            <span className={styles.category}>Capital: </span>
+            {capital}
+          </span>
+        </div>
+      </div>
+    </Link>
+  );
+};
+
+export { CountryCard };
