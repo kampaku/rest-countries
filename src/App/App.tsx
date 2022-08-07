22f5ec3e1981a841ce35dc07a04fe00cdd404465
Kampaku <kampaku@users.noreply.github.com>
2022-08-07 18:31:01 +0300

    add router

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,21 +1,20 @@
 import '../assets/scss/style.scss';
 
 import { useEffect } from 'react';
-import { Route,Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 
 import { Header } from '../components';
-import { CountryPage,SearchPage } from '../pages';
+import { CountryPage, SearchPage } from '../pages';
 import styles from './App.module.scss';
 
 const App = () => {
-
   return (
     <div className={styles.app}>
       <Header />
       <div className={styles.container}>
         <Routes>
-          <Route path='/' element={<SearchPage />}/>
-          <Route path='/country/:name' element={<CountryPage />}/>
+          <Route path="/" element={<SearchPage />} />
+          <Route path="/country/:name" element={<CountryPage />} />
         </Routes>
       </div>
     </div>
