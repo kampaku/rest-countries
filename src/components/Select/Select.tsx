423a11f07488b811e7286254c979f34bfa4054b9
Kampaku <kampaku@users.noreply.github.com>
2022-08-07 18:52:14 +0300

    add select

---
+++ src/components/Select/Select.tsx
@@ -0,0 +1,35 @@
+import { FC, useEffect, useState } from 'react';
+
+import styles from './Select.module.scss';
+
+type Props = {
+  onSelect: (region: string) => void;
+};
+const Select: FC<Props> = ({ onSelect }) => {
+  const [value, setValue] = useState('all');
+
+  const onChange = (val: string) => {
+    setValue(val);
+  };
+  useEffect(() => {
+    onSelect(value);
+    // console.log(value);
+  }, [onSelect, value]);
+  return (
+    <div>
+      <select
+        className={styles.select}
+        onChange={(e) => onChange(e.target.value)}
+      >
+        <option value="all">All</option>
+        <option value="africa">Africa</option>
+        <option value="america">America</option>
+        <option value="asia">Asia</option>
+        <option value="europe">Europe</option>
+        <option value="oceania">Oceania</option>
+      </select>
+    </div>
+  );
+};
+
+export { Select };
